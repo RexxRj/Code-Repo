d5abd14d91bc2fd990176dbb2e653bd92fc14a65
RexxRj <103634570+RexxRj@users.noreply.github.com>
2024-09-23 23:08:59 +0530

    spaced optimised solution for longest palindrome substring

---
+++ Leetcode/LC-Longest-Palindromic-Substring-Expand-From-Center-Space-Optimised.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+private:
+    string expand(int i, int j, string &s)
+    {
+        int left = i, right = j;
+
+        while (left >= 0 && right < s.length() && s[left] == s[right])
+        {
+            left--;
+            right++;
+        }
+
+        return s.substr(left + 1, right - left - 1);
+    }
+
+public:
+    string longestPalindrome(string s)
+    {
+
+        string ans = "";
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            string odd = expand(i, i, s);
+            if (odd.length() > ans.length())
+                ans = odd;
+
+            string even = expand(i, i + 1, s);
+            if (even.length() > ans.length())
+                ans = even;
+        }
+
+        return ans;
+    }
+};
