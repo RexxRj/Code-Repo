5b1aa39890901512a3b1dc51e531b193ece48f79
RexxRj <103634570+RexxRj@users.noreply.github.com>
2024-09-30 22:46:04 +0530

    Two Sum with BST Iterator Class

---
+++ Solved/LC-Two-Sum-IV-Class-BST-Iterator.cpp
@@ -0,0 +1,79 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator
+{
+private:
+    stack<TreeNode *> st;
+    bool reverse = false;
+
+    void pushAll(TreeNode *root)
+    {
+        while (root != NULL)
+        {
+            st.push(root);
+            if (reverse)
+                root = root->right;
+            else
+                root = root->left;
+        }
+    }
+
+public:
+    BSTIterator(TreeNode *root, bool isReverse)
+    {
+        reverse = isReverse;
+        pushAll(root);
+    }
+
+    int next()
+    {
+
+        TreeNode *temp = st.top();
+        st.pop();
+
+        if (reverse && temp->left)
+            pushAll(temp->left);
+        else if (!reverse && temp->right)
+            pushAll(temp->right);
+
+        return temp->val;
+    }
+};
+
+class Solution
+{
+public:
+    bool findTarget(TreeNode *root, int k)
+    {
+
+        if (root == NULL)
+            return false;
+
+        BSTIterator l(root, false);
+        BSTIterator r(root, true);
+
+        int i = l.next();
+        int j = r.next();
+
+        while (i < j)
+        {
+            if (i + j == k)
+                return true;
+            else if (i + j < k)
+                i = l.next();
+            else
+                j = r.next();
+        }
+
+        return false;
+    }
+};
